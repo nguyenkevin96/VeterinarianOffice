703e277e7dcab3abf35f33e7ad8ef68c050698f2
Kevin <kknguy24@gmail.com>
2019-10-22 22:27:15 -0500

    Added Owner scaffold

---
+++ db/migrate/20191023032605_create_owners.rb
@@ -0,0 +1,12 @@
+class CreateOwners < ActiveRecord::Migration[6.0]
+  def change
+    create_table :owners do |t|
+      t.string :fname
+      t.string :lname
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
