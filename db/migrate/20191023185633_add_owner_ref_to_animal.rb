9f64b1e4f4ec5fba587f9b7efb2cec3cb5f5a535
Kevin <kknguy24@gmail.com>
2019-10-26 17:24:28 -0500

    Fixed Migrations

--- db/migrate/20191023185633_add_owner_ref_to_animal.rb
+++ db/migrate/20191023185633_add_owner_ref_to_animal.rb
@@ -1,5 +1,5 @@
 class AddOwnerRefToAnimal < ActiveRecord::Migration[6.0]
   def change
-    add_reference :animals, :owner, null: false, foreign_key: true
+    add_reference :animals, :owner, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_033706) do
+ActiveRecord::Schema.define(version: 2019_10_23_185633) do
 
   create_table "animals", force: :cascade do |t|
     t.string "name"
     t.string "breed"
     t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "owner_id"
+    t.index ["owner_id"], name: "index_animals_on_owner_id"
+  end
+
+  create_table "dateofinfections", force: :cascade do |t|
+    t.datetime "dateofinfection"
+    t.integer "animal_id", null: false
+    t.integer "disease_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["animal_id"], name: "index_dateofinfections_on_animal_id"
+    t.index ["disease_id"], name: "index_dateofinfections_on_disease_id"
+  end
+
+  create_table "diseases", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -29,4 +48,7 @@ ActiveRecord::Schema.define(version: 2019_10_23_033706) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "animals", "owners"
+  add_foreign_key "dateofinfections", "animals"
+  add_foreign_key "dateofinfections", "diseases"
 end
